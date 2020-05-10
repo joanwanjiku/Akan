55d24ae869033897b749c9e060f01cbccafc8116
Joan_E <joanevans18@gmail.com>
2020-05-10 14:55:42 +0300

    Edited a for loop

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ const validateDate = () => {
     let date = document.getElementById('date').value;
     let radioBtns = document.querySelectorAll('input[name="gridRadios"]');
     let selectedGender;
-    for (let radiobtn of radioBtns){
-        if (radiobtn.checked) {
+    radioBtns.forEach(radiobtn => {
+        if(radiobtn.checked) {
             selectedGender = radiobtn.value
         }
-    }
+    })    
     let errorMsg = document.getElementById('error');
     if (date === "" || selectedGender === undefined){
         errorMsg.classList.add('error');
